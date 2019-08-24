55290faa136773261d791a608b94d43d6f868b1f
yc791 <yc791@scarletmail.rutgers.edu>
2019-08-24 04:12:39 -0400

    Pharmacy google maps nearby search api call

--- OnlineDoctor/screens/Pharmacy.js
+++ OnlineDoctor/screens/Pharmacy.js
@@ -1,45 +1,90 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Alert, Platform, Image } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
 
 class Pharmacy extends Component {
 
   constructor(props) {
     super(props);
-
     this.state = {
-      latitude: null,
-      longitude: null,
-      error:null,
-    };
+      lat : 0,
+      long : 0,
+      error : null,
+      places: null
+    }
   }
 
-  // componentDidMount() {
-  //   navigator.geolocation.getCurrentPosition(
-  //     (position) => {
-  //       this.setState({
-  //         latitude: position.coords.latitude,
-  //         longitude: position.coords.longitude,
-  //         error: null,
-  //       }); 
-  //     },
-  //     (error) => this.setState({ error: error.message }),
-  //     { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
-  //   );
-  // }
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        this.setState({
+          lat,
+          long,
+          error : null
+        })
+        this.getPlaces();
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 200000, maximumAge: 1000 },
+    );
+  }
 
+  getPlaces(){
+    const url = this.getUrlWithParameters(this.state.lat, this.state.long, 1500, 'pharmacy', 'AIzaSyDxTBLrmNG5wFleo5sWk8PGK4uHrEIIvCs');
+    console.log('123: ' + url);
+    fetch(url)
+      .then((data) => data.json())
+      .then((res) => {
+        const arrayMarkers = [];
+        res.results.map(( element, i ) => {
+          arrayMarkers.push(
+            <Marker
+              key={i}
+              coordinate={{
+                latitude: element.geometry.location.lat,
+                longitude: element.geometry.location.lng
+              }}
+            >
+              <Callout>
+                <View>
+                  <Text>
+                    {element.name}
+                  </Text>
+                </View>
+              </Callout>
+            </Marker>
+          )
+        })
+        this.setState({ places: arrayMarkers });
+      })
+  }
+
+  getUrlWithParameters(lat, long, radius, type, API){
+    const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?';
+    const location = `location=${lat},${long}&radius=${radius}`;
+    const typeData = `&type=${type}`;
+    const key = `&key=${API}`;
+    return `${url}${location}${typeData}${key}`;
+  }
+  
   render() {
     return (
       <MapView
         style={styles.map}
-        initialRegion={{
-          latitude: this.state.latitude,
-          longitude: this.state.longitude,
-          latitudeDelta: 1,
-          longitudeDelta: 1,
+        provider={PROVIDER_GOOGLE}
+        region={{
+          latitude: this.state.lat,
+          longitude: this.state.long,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
-      />
-    )
+      >
+        {this.state.places}
+      </MapView>
+    );
   }
 }
 export default Pharmacy;
